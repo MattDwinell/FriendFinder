3e469b60be806fa6cc0e3b62d0f4a5ced045f643
Matt D <dwinell.matt@gmail.com>
2019-06-12 17:39:23 -0400

    added picture urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,10 +2,12 @@
 var friendsArray= [
   {
       name: 'Bubba',
+      picture: 'https://i.pinimg.com/originals/7c/6e/75/7c6e75e793a2685f61536aec7981025e.gif',
       answers:['5' , '3', '2', '2', '1', '3', '2', '4', '5', '1']
   },
   {
     name: 'Raisins',
+    picture: 'https://en.wikipedia.org/wiki/Raisin#/media/File:Sunmaid-Raisin-Pile.jpg',
     answers:['1' , '2', '3', '4', '4', '4', '5', '5', '5', '5']
 }
   ];
