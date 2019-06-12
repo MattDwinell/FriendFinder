3d632a83a21d92bed76875203a8c81ffa2210d6d
Matt D <dwinell.matt@gmail.com>
2019-06-12 12:00:14 -0400

    coded function to determine most similar friend

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+
+var friendsArray= [
+  {
+      name: 'Bubba',
+      answers:['5' , '3', '2', '2', '1', '3', '2', '4', '5', '1']
+  },
+  {
+    name: 'Raisins',
+    answers:['1' , '2', '3', '4', '4', '4', '5', '5', '5', '5']
+}
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
